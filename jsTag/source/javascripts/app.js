a2a7c1b046795b594c2f72ba8c2739d2923300ca
Eran H <eranhirs@mail.mta.ac.il>
2013-12-24 01:28:19 +0200

    Adding files

---
+++ jsTag/source/javascripts/app.js
@@ -0,0 +1,13 @@
+var jsTag = angular.module('jsTag', []);
+
+// Defaults for jsTag (can be overriden as shown in example)
+jsTag.constant('jsTagDefaults', {
+	'defaultTags': [],
+	'breakCodes': [
+		13, // Return
+		44 // Comma
+	],
+	'texts': {
+		'inputPlaceHolder': "Input text"
+	}
+});
---
+++ jsTag/source/javascripts/models/default/jsTag.js
@@ -0,0 +1,11 @@
+var jsTag = angular.module('jsTag');
+
+// Tag Model
+jsTag.factory('JSTag', function() {
+	function JSTag(value, id) {
+		this.value = value;
+		this.id = id;
+	}
+	
+	return JSTag;
+});

7b8298c0593bad062758733b735e069326a00858
Eran H <eranhirs@mail.mta.ac.il>
2013-12-25 22:05:19 +0200

    Removed assets from master branch, needed only in gh-pages branch

--- assets/index.js
+++
@@ -1,8 +0,0 @@
-var demoApp = angular.module('demoApp', ['jsTag']);
-
-demoApp.controller('BasicDemoCtrl', ['$scope', function($scope) {
-}]);
-
-demoApp.controller('AdvancedDemoCtrl', ['$scope', function($scope) {
-}]);
-

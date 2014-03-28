14e244240ff762d1ccedb9477b90aa4172c836fa
Eran H <eranhirs@mail.mta.ac.il>
2014-03-28 12:08:41 +0300

    Removed typeahead from

--- assets/index.js
+++ assets/index.js
@@ -25,13 +25,13 @@ demoApp.controller('TypeaheadController', ['$scope', 'JSTagsCollection', functio
   $scope.tags = new JSTagsCollection(["jsTag", "angularJS"]);
   
   // Build list of suggestions
-  $scope.typeahead = {
+  var typeahead = {
     suggestions: ['jsTag', 'c#', 'java', 'javascript', 'jquery', 'android' , 'php', 'c++', 'python', 'c++', 'asp.net', 'ios', 'mysql', 'iphone', 'sql', 'html', 'css', 'objective-c', 'ruby-on-rails', 'c', 'sql-server', 'ajax', 'xml', '.net', 'ruby', 'regex', 'database', 'vb.net', 'arrays', 'eclipse', 'json', 'django', 'linux', 'xcode', 'windows', 'html5', 'winforms', 'r', 'wcf', 'visual-studio-2010', 'forms', 'performance', 'excel', 'spring', 'node.js', 'git', 'apache', 'entity-framework', 'asp.net', 'web-services', 'linq', 'perl', 'oracle', 'action-script', 'wordpress', 'delphi', 'jquery-ui', 'tsql', 'mongodb', 'neo4j', 'angularJS', 'unit-testing', 'postgresql', 'scala', 'xaml', 'http', 'validation', 'rest', 'bash', 'css', 'django', 'silverlight', 'cake-php', 'elgg', 'oracle', 'cocoa', 'swing', 'mocha', 'amazon-web-services']
   };
 
   // Export jsTags options, inlcuding our own tags object
   $scope.jsTagOptions = {
     "tags": $scope.tags,
-    "typeahead": $scope.typeahead
+    "typeahead": typeahead
   };
 }]);

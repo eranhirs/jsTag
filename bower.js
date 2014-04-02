68700679086e75a7acb7e18f2ba075ece6adcabb
Eran H <eranhirs@mail.mta.ac.il>
2014-04-03 01:34:45 +0300

    Add bower.js file

---
+++ bower.js
@@ -0,0 +1,21 @@
+{
+	"name": "jsTag",
+	"version": "0.2.1",
+	"keywords": [
+		"jstag",
+		"js-tag",
+		"tags",
+		"angular",
+		"angularjs"
+	],
+	"author": "eranhirs",
+	"license": "MIT",
+	"main": ["jsTag/compiled/jsTag.min.js", "jsTag/compiled/jsTag.css"],
+	"ignore": [
+			"jsTag/source",
+			".gitignore",
+			"GruntFile.js",
+			"package.json",
+			"README.md"
+	]
+}

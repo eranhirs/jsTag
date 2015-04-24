9d5578a1d9063b0df20f25a97f86a163e6ffb7be
danielronnkvist <daniel@ronnkvist.com>
2015-04-24 08:46:09 +0200

    Added a method to the tag collection to be able to get all values
    Also updated the README with some configuration guidance. I felt it was necessary to have the getTagValues function since it was the easiest way to get all values.
    And since there isn't any documentation on how to customize even though it says it's higly customizable was irritating. Hopefully with the documentation it will be a bit more plug and play for people. Without having to go through the source code.

--- jsTag/source/javascripts/models/default/jsTagsCollection.js
+++ jsTag/source/javascripts/models/default/jsTagsCollection.js
@@ -2,7 +2,7 @@ var jsTag = angular.module('jsTag');
 
 // TagsCollection Model
 jsTag.factory('JSTagsCollection', ['JSTag', '$filter', function(JSTag, $filter) {
-  
+
   // Constructor
   function JSTagsCollection(defaultTags) {
     this.tags = {};
@@ -11,30 +11,30 @@ jsTag.factory('JSTagsCollection', ['JSTag', '$filter', function(JSTag, $filter) 
       var defaultTagValue = defaultTags[defaultTagKey];
       this.addTag(defaultTagValue);
     }
-   
+
     this._onAddListenerList = [];
     this._onRemoveListenerList = [];
- 
+
     this.unsetActiveTags();
     this.unsetEditedTag();
   }
-  
+
   // *** Methods *** //
-  
+
   // *** Object manipulation methods *** //
-  
+
   // Adds a tag with received value
   JSTagsCollection.prototype.addTag = function(value) {
     var tagIndex = this.tagsCounter;
     this.tagsCounter++;
-  
+
     var newTag = new JSTag(value, tagIndex);
     this.tags[tagIndex] = newTag;
     angular.forEach(this._onAddListenerList, function (callback) {
       callback(newTag);
     });
   }
-  
+
   // Removes the received tag
   JSTagsCollection.prototype.removeTag = function(tagIndex) {
     var tag = this.tags[tagIndex];
@@ -56,7 +56,16 @@ jsTag.factory('JSTagsCollection', ['JSTag', '$filter', function(JSTag, $filter) 
   JSTagsCollection.prototype.getNumberOfTags = function() {
     return getNumberOfProperties(this.tags);
   }
-  
+
+  // Returns an array with all values of the tags
+  JSTagsCollection.prototype.getTagValues = function() {
+    var tagValues = [];
+    for (var tag in this.tags) {
+      tagValues.push(this.tags[tag].value);
+    }
+    return tagValues;
+  }
+
   // Returns the previous tag before the tag received as input
   // Returns same tag if it's the first
   JSTagsCollection.prototype.getPreviousTag = function(tag) {
@@ -68,7 +77,7 @@ jsTag.factory('JSTagsCollection', ['JSTag', '$filter', function(JSTag, $filter) 
       return getPreviousProperty(this.tags, tag.id);
     }
   }
-  
+
   // Returns the next tag after  the tag received as input
   // Returns same tag if it's the last
   JSTagsCollection.prototype.getNextTag = function(tag) {
@@ -80,21 +89,21 @@ jsTag.factory('JSTagsCollection', ['JSTag', '$filter', function(JSTag, $filter) 
       return getNextProperty(this.tags, tag.id);
     }
   }
-  
+
   // *** Active methods *** //
-  
+
   // Checks if a specific tag is active
   JSTagsCollection.prototype.isTagActive = function(tag) {
     return $filter("inArray")(tag, this._activeTags);
   };
-  
+
   // Sets tag to active
   JSTagsCollection.prototype.setActiveTag = function(tag) {
     if (!this.isTagActive(tag)) {
       this._activeTags.push(tag);
     }
   };
-  
+
   // Sets the last tag to be active
   JSTagsCollection.prototype.setLastTagActive = function() {
     if (getNumberOfProperties(this.tags) > 0) {
@@ -102,49 +111,49 @@ jsTag.factory('JSTagsCollection', ['JSTag', '$filter', function(JSTag, $filter) 
       this.setActiveTag(lastTag);
     }
   };
-  
+
   // Unsets an active tag
   JSTagsCollection.prototype.unsetActiveTag = function(tag) {
     var removedTag = this._activeTags.splice(this._activeTags.indexOf(tag), 1);
   };
-  
+
   // Unsets all active tag
   JSTagsCollection.prototype.unsetActiveTags = function() {
     this._activeTags = [];
   };
-  
+
   // Returns a JSTag only if there is 1 exactly active tags, otherwise null
   JSTagsCollection.prototype.getActiveTag = function() {
     var activeTag = null;
     if (this._activeTags.length === 1) {
       activeTag = this._activeTags[0];
     }
-    
+
     return activeTag;
   };
-  
+
   // Returns number of active tags
   JSTagsCollection.prototype.getNumOfActiveTags = function() {
     return this._activeTags.length;
   };
-  
+
   // *** Edit methods *** //
-  
+
   // Gets the edited tag
   JSTagsCollection.prototype.getEditedTag = function() {
     return this._editedTag;
   };
-  
+
   // Checks if a tag is edited
   JSTagsCollection.prototype.isTagEdited = function(tag) {
     return tag === this._editedTag;
   };
-  
+
   // Sets the tag in the _editedTag member
   JSTagsCollection.prototype.setEditedTag = function(tag) {
     this._editedTag = tag;
   };
-  
+
   // Unsets the 'edit' flag on a tag by it's given index
   JSTagsCollection.prototype.unsetEditedTag = function() {
     // Kill empty tags!
@@ -153,10 +162,10 @@ jsTag.factory('JSTagsCollection', ['JSTag', '$filter', function(JSTag, $filter) 
         this._editedTag.value === "") {
       this.removeTag(this._editedTag.id);
     }
-    
+
     this._editedTag = null;
   }
-  
+
   return JSTagsCollection;
 }]);
 
